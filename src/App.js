32ec0377238e89c071dcd6f416f58475cb969b70
Camila <salescamila1@gmail.com>
2020-05-05 23:26:09 -0400

    Estado e imutabilidade [1]

--- src/App.js
+++ src/App.js
@@ -3,21 +3,23 @@ import React from 'react';
 import Header from './components/Header';
 
 function App() {
+  const projects = ['Desenvolvimento de app', 'Front-end web'];
+
+  function handleAddProject() {
+    projects.push(`Novo projeto ${Date.now()}`);
+
+    console.log(projects);
+  }
+
   return (
     <>
-      <Header title="Homepage">
-        <ul>
-          <li>Homepage</li>
-          <li>Projects</li>
-        </ul>
-      </Header>
-      <Header title="Projects">
+      <Header title="Projects" />
+
       <ul>
-          <li>Homepage</li>
-          <li>Projects</li>
-          <li>Login</li>
-        </ul>
-      </Header>
+        {projects.map(project => <li key={project}>{project}</li>)}
+      </ul>
+
+      <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
     </>
   );
 }
