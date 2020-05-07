76b059f4fd98828bd3809635f275debbaf2e2fec
Camila <salescamila1@gmail.com>
2020-05-06 22:36:56 -0400

    Importando CSS e imagens

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 
+import './App.css';
+import backgroundImage from './assets/background.jpeg';
+
 import Header from './components/Header';
 
 /**
@@ -29,6 +32,8 @@ function App() {
   return (
     <>
       <Header title="Projects" />
+
+      <img width={350} src={backgroundImage} alt=""/>
 
       <ul>
         {projects.map(project => <li key={project}>{project}</li>)}
